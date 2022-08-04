de1a28b7c703e7d981f8814e75a54fd113022c12
kuma.jung <yj.jung5078@gmail.com>
2022-08-04 17:46:50 +0900

    modify

--- lguctl.rb
+++ lguctl.rb
@@ -1,7 +1,7 @@
 class Lguctl < Formula
-  homepage "https://github.com/u-cto-lguctl/lguctl"
-#  url ""
-#  sha256 ""
+  homepage "https://github.com/svc-proto-dev/lguctl"
+  url "https://uplus-cto-devops-files.s3.ap-northeast-2.amazonaws.com/lguctl/releases/0.0.1/lguctl"
+  sha256 "45fcfc0643109939f238e56c22db18938b27ed71c7c5dbf9d7e83456b62578ba"
 
   def install
     bin.install "lguctl"

428422e9d377b80cb69286fbd21f6a29587b1fcf
YoungJinJung <yj.jung5078@gmail.com>
2022-09-08 10:22:35 +0900

    update version to 0.0.2

--- lguctl.rb
+++ lguctl.rb
@@ -1,7 +1,7 @@
 class Lguctl < Formula
   homepage "https://github.com/svc-proto-dev/lguctl"
-  url "https://uplus-cto-devops-files.s3.ap-northeast-2.amazonaws.com/lguctl/releases/0.0.1/lguctl"
-  sha256 "45fcfc0643109939f238e56c22db18938b27ed71c7c5dbf9d7e83456b62578ba"
+  url "https://uplus-cto-devops-files.s3.ap-northeast-2.amazonaws.com/lguctl/releases/0.0.2/lguctl"
+  sha256 "e357fe3465b0b2cf78237315b212fe7295c7e519755befd052dfece14adddf4e"
 
   def install
     bin.install "lguctl"

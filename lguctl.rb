fb6089ad192526ab9d7e634cae53acb51bbc7dc6
kuma.jung <yj.jung5078@gmail.com>
2022-12-09 11:18:24 +0900

    modify sha256

--- lguctl.rb
+++ lguctl.rb
@@ -1,7 +1,7 @@
 class Lguctl < Formula
   homepage "https://github.com/svc-proto-dev/lguctl"
   url "https://uplus-cto-devops-files.s3.ap-northeast-2.amazonaws.com/lguctl/releases/0.0.2/lguctl"
-  sha256 "e357fe3465b0b2cf78237315b212fe7295c7e519755befd052dfece14adddf4e"
+  sha256 "bb6873298aca49fa82d09e344ce30fa405f87a0d4aafca3437c0433eecf893b6"
 
   def install
     bin.install "lguctl"
